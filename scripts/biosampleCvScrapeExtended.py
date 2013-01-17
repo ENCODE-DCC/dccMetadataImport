2882b34977d75afe1e6271aa3460538d25e26aed
Jeffrey Long <jefflong@stanford.edu>
2013-01-17 12:50:36 -0800

    Script to scrape biosample from a local fixed version of cv.ra

---
+++ scripts/biosampleCvScrapeExtended.py
@@ -0,0 +1,36 @@
+import sys, string
+import re
+from ucscGb.gbData.ordereddict import OrderedDict
+from ucscGb.gbData.ra.raFile import RaFile
+from ucscGb.gbData import ucscUtils
+import collections
+
+
+rafile = RaFile('/cluster/home/jlong/stanford/cvJan15.ra')
+metaObject = 0
+expId = 0
+term = 0
+type = 0
+
+for key in rafile.keys():
+    thisstanza = rafile[key]
+
+
+    if thisstanza['type'] == 'Cell Line':
+        if 'category' not in thisstanza:	
+            thisstanza['category'] = 'missing'
+        if 'treatment' not in thisstanza:	
+            thisstanza['treatment'] = 'missing'
+        if 'lots' not in thisstanza:
+            thisstanza['lots'] = 'missing'
+        if 'subcellularLoc' not in thisstanza:	
+            thisstanza['subcellularLoc'] = 'missing'
+        if 'tier' not in thisstanza:
+            thisstanza['tier'] = 'missing'
+        if 'age' not in thisstanza:
+            thisstanza['age'] = 'missing'
+        if 'orderUrl' not in thisstanza:	
+            thisstanza['orderUrl'] = 'missing'
+        if 'karyotype' not in thisstanza:	
+            thisstanza['karyotype'] = 'missing'
+        print thisstanza['termId'],"\t",thisstanza['term'],"\t",thisstanza['tag'],"\t",thisstanza['category'],"\t",thisstanza['organism'],"\t",thisstanza['treatment'],"\t",thisstanza['vendorName'],"\t",thisstanza['vendorId'],"\t",thisstanza['lots'],"\t",thisstanza['subcellularLoc'],"\t",thisstanza['orderUrl'],"\t",thisstanza['sex'],"\t",thisstanza['age'],"\t",thisstanza['protocol'],"\t",thisstanza['karyotype'],"\t",thisstanza['tier']
