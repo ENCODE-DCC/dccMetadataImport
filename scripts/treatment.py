ddec4c0fd1f90d81c63c10d6cb8920dbc339d666
Jeffrey Long <jefflong@stanford.edu>
2013-01-17 14:24:38 -0800

    fixing bug

---
+++ scripts/treatment.py
@@ -0,0 +1,19 @@
+import sys, string
+import re
+from ucscGb.gbData.ordereddict import OrderedDict
+from ucscGb.gbData.ra.raFile import RaFile
+from ucscGb.gbData import ucscUtils
+import collections
+
+
+rafile = RaFile('../data/cvJan15.ra')
+treatment = 0
+
+for key in rafile.keys():
+    thisstanza = rafile[key]
+
+
+    if thisstanza['type'] == 'treatment':
+        if 'treatment' not in thisstanza:
+            thisstanza['treatment'] = 'missing'
+        print thisstanza['treatment']
