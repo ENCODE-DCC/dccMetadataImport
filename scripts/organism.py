338717df1a4008e81cafa2da73ed2a48458cebad
Jeffrey Long <jefflong@stanford.edu>
2013-01-22 16:05:34 -0800

    Update scripts/organism.py
    Adding bash script to comments

--- scripts/organism.py
+++ scripts/organism.py
@@ -17,4 +17,10 @@ for key in rafile.keys():
 
     if thisstanza['type'] == 'Cell Line':
         if thisstanza['organism'] == 'mouse':
-            print "mouse\tMus\tmusculus\t",thisstanza['strain'],"\t",thisstanza['term'],"10090"
+            #print "mouse\tMus\tmusculus\t",thisstanza['strain'],"\t",thisstanza['term'],"10090"
+            print "mouse\tMus\tmusculus\t",thisstanza['strain'],"\tnull\t","10090"
+            #the listOfAllMouseTerms came from this script just printing thisstanza['term'],",\n"
+            #queried metaDb with a bash script for buried strains
+            #where MICE= "listOfAllMouseTerms"
+            #for i in $MICE; do /cluster/bin/x86_64/mdbPrint mm9 -vars="cell=$i" | grep 'strain' | sort -u >> ORGANISM_STRAINS.txt; done
+            #added in the few "buried strains"            
