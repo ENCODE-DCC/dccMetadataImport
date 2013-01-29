939b3ed84fefed14751a2083236a579871253cd4
Jeffrey Long <jefflong@stanford.edu>
2013-01-29 11:07:36 -0800

    Adding script that got the majority of biosample documents

--- scripts/document.py
+++ scripts/document.py
@@ -8,27 +8,25 @@ import collections
 
 rafile = RaFile('../data/cvJan15.ra')
 protocol = None
-description = None
-validation = None
-antibodyDescription = None
-targetDescription = None
-label = None
-term = None
-vendorName = None
-vendorId = None
+protocolPhrase = None
+source = None
+fileName = None
+cell = None
 
 for key in rafile.keys():
     thisstanza = rafile[key]
 
     if thisstanza['type'] == 'Cell Line':
         if 'protocol' not in thisstanza:
-            thisstanza['protocol'] = 'NA'
-        print thisstanza['protocol'],"\tBIOSAMPLE_DOCUMENT\t",thisstanza['term'],thisstanza['vendorName'],thisstanza['vendorId'],thisstanza['description']
+            thisstanza['protocol'] = 'null'
+            continue
+        protocolPhrase = thisstanza['protocol'].split()
+        for i in range (0, len(protocolPhrase)):
+            if 'missing' not in protocolPhrase[i]:
+                source = protocolPhrase[i].split(":")
+                cell = source[1].split("_")
 
-    if thisstanza['type'] == 'Antibody':
-        if 'validation' not in thisstanza:
-            thisstanza['validation'] = 'NA'
-        if 'label' not in thisstanza:
-            thisstanza['label'] = 'NA'
-        print thisstanza['validation'],"\tANTIBODY_DOCUMENT\t",thisstanza['label'],"  antibodyDescription  ",thisstanza['antibodyDescription'],"  targetDescription  ",thisstanza['targetDescription']
-
+        print source[1],"\tBIOSAMPLE_DOCUMENT\t",cell[0],"protocol\t","Future UCSD address","\tProtocol for the",cell[0],"biosample from",source[0]
+        
+            #Stam:HBMEC_Stam_protocol.pdf
+        
